09be37a0ed452b50db730923abf86d39ed026b75
Le <duraesleandro21@gmail.com>
2023-08-24 22:36:25 -0300

    Update Main.java

--- T4/NetBeans-srcCorrigido/Calculadora-Geometrica/src/main/java/com/calculadora/geometrica/main/Main.java
+++ T4/NetBeans-srcCorrigido/Calculadora-Geometrica/src/main/java/com/calculadora/geometrica/main/Main.java
@@ -11,9 +11,9 @@ public class Main {
         Scanner ler = new Scanner(System.in);
         System.out.println("Sistema de Cálculos Geométricos");
         Ishape figure = null;
-
+        
         do{
-            System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n");
+            
             System.out.println("Selecione uma das opções a seguir:");
             System.out.println("[1] - Criar Figura");
             System.out.println("[2] - Realizar operações geométricas");
@@ -27,19 +27,38 @@ public class Main {
                 System.out.println("[2] - Circulo");
                 System.out.println("[3] - Triângulo Retângulo");
                 System.out.print("Opção: ");
-                opt = ler.nextInt();
-                if(opt == 1){
-                    System.out.print("Digite os valores de x e y do quadrilátero: ");
-                    double x = ler.nextDouble();
-                    double y = ler.nextDouble();
-                    figure = new Quadrilatero(x,y);
+                //opt = 
+                switch (ler.nextInt()) {
+                    case 1:
+                        System.out.print("Digite os valores de x e y do quadrilátero: ");
+                        figure = new Quadrilatero(ler.nextDouble(), ler.nextDouble());
+                        break;
+                    case 2:
+                        System.out.print("Digite o valor do raio do círculo: ");
+                        figure = new Circulo(ler.nextDouble());
+                        break;
+                    case 3:
+                        System.out.print("Digite o valor dos catetos e da hipotenusa: ");
+                        figure = new TrianguloRetangulo(ler.nextDouble(),ler.nextDouble(),ler.nextDouble());
+                        break;
+                    default:
+                        break;
                 }
-                else if(opt == 2){
-                    System.out.print("Digite o valor do raio do círculo: ");
-                    double r = ler.nextDouble();
-                    figure = new Circulo(r);
+                System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n");
+                System.out.println("Figura Criada com sucesso");
+            }
+            
+            else if(opt == 2){
+                System.out.print("\nFigura selecionada atualmente: ");
+                if(figure instanceof Quadrilatero){
+                    System.out.println("Quadrilátero");
+                } else if(figure instanceof Circulo){
+                    System.out.println("Círculo");
+                } else if(figure instanceof TrianguloRetangulo){
+                    System.out.println("Triângulo Retângulo");
                 }
                 
+                System.out.println("\n\nO que deseja calcular da figura?");
             }
         }while(opt != 5);
 
